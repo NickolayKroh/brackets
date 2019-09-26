3459cad1bee0e2e81f3bf918ebb3de56c6309d65
Kroh Nickolay <mikolka.del@gmail.com>
2019-09-26 20:45:48 +0300

    task complited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  
+  let getClosingBracket = (strPosition) => {
+    for(let i = 0; i < bracketsConfig.length; ++i)
+      if(str[strPosition] === bracketsConfig[i][0])
+        return bracketsConfig[i][1];
+    return -1;
+  }
+  
+  let isClosingBracket = (strPosition) => {
+    for(let i = 0; i < bracketsConfig.length; ++i)
+      if(str[strPosition] === bracketsConfig[i][0])
+        return false;
+    return true;
+  }
+  
+  for(let cursor = 0; cursor < str.length; ++cursor) {
+    if(stack.length && str[cursor] === getClosingBracket(stack[stack.length - 1])  ) {
+      stack.pop();
+    } else {
+      if(isClosingBracket(cursor)) {
+        return false;
+      } else {
+        stack.push(cursor);
+      }
+    }
+  }
+  
+  if(stack.length)
+    //haven't got a pair for bracket
+    return false;
+     
+  return true;
 }
+
+// console.log( check('()', [['(', ')']]) ); // -> true
+// console.log( check('((()))()', [['(', ')']]) ); // -> true
+// console.log( check('())(', [['(', ')']]) ); // -> false
+// console.log( check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) ); // -> true
+// console.log( check('[(])', [['(', ')'], ['[', ']']]) ); // -> false
+// console.log( check('[]()', [['(', ')'], ['[', ']']]) ); // -> true
+// console.log( check('[]()(', [['(', ')'], ['[', ']']]) ); // -> false
+// console.log(' ');
+// console.log( check('||', [['|', '|']]) ); // -> true
+// console.log( check('|()|', [['(', ')'], ['|', '|']]) ); // -> true
+// console.log( check('|(|)', [['(', ')'], ['|', '|']]) ); // -> false
+// console.log( check('|()|(||)||', [['(', ')'], ['|', '|']]) ); // -> true
+
+
+// console.log(' ');
+// console.log( check('))', [['(', ')']]) ); // -> false
+
